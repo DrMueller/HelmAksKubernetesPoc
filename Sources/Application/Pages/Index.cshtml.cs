f3243c47d6e6476eafe33d9955c78bdfcf982732
Matthias Müller <matthias.mueller@trivadis.com>
2022-06-21 13:09:24 +0200

    WIP

--- Sources/Application/Pages/Index.cshtml.cs
+++ Sources/Application/Pages/Index.cshtml.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace HelmAksKubernetesPoc.Pages
+namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
