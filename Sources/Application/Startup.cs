e3aaa3dd6062c94ddd893ecad56c8dae2874aaa0
Matthias Müller <matthias.mueller@trivadis.com>
2022-07-04 09:38:34 +0200

    WIP

--- Sources/Application/Startup.cs
+++ Sources/Application/Startup.cs
@@ -37,6 +37,8 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase("dev");
+
             app.UseMiddleware<RouteMiddleware>();
 
             if (env.IsDevelopment())
@@ -61,7 +63,6 @@ namespace WebApplication1
             app.UseEndpoints(
                 endpoints =>
                 {
-
                     endpoints.MapRazorPages();
                 });
         }
