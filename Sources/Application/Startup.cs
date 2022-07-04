8ffb6ae7782715de7c5fdedc00c385c1cef4ba77
Matthias Müller <matthias.mueller@trivadis.com>
2022-07-04 09:47:14 +0200

    WIP

--- Sources/Application/Startup.cs
+++ Sources/Application/Startup.cs
@@ -37,7 +37,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UsePathBase("dev");
+            app.UsePathBase("/dev");
 
             app.UseMiddleware<RouteMiddleware>();
 
